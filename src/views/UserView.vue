81c465e7f5e492b40c71136977b0e0aecd534228
rucev <ruedaflors@gmail.com>
2023-01-29 01:48:53 +0100

    Change UserView style

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -9,7 +9,9 @@ const postsStore = usePosts()
 
 let userData = ref({
     avatar:"",
-    name: ""
+    name: "",
+    email: "",
+    role: ""
 });
 
 axios.get("https://api.escuelajs.co/api/v1/auth/profile", {
@@ -28,16 +30,27 @@ axios.get("https://api.escuelajs.co/api/v1/auth/profile", {
 
 <template>
   <NavBar/>
-  <div class="card mb-3" style="width 50rem;">
+  <div class="card border-dark mb-3 mx-auto" style="max-width: 90%; margin: 1% 0%">
     <div class="row no-gutters">
     <div class="col-md-4">
       <img v-bind:src="userData.avatar" class="card-img" alt="your picture">
     </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{{ userData.name }}</h5>
+    <div class="col-md-8 text-position">
+      <div class="card-body ">
+        <h5 class="card-title">NAME: {{ userData.name }}</h5>
+        <h5 class="card-title">MAIL: {{ userData.email }}</h5>
+        <h5 class="card-title">ROLE: {{ userData.role }}</h5>
       </div>
     </div>
   </div>
   </div>
 </template>
+
+<style scoped>
+.text-position{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding-top: 5%;
+}
+</style>
