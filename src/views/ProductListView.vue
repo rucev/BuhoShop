8951c858cd79d813fcf76c4988c3ef1ff5cd56b2
rucev <ruedaflors@gmail.com>
2023-01-29 01:15:25 +0100

    Change ProductListView Style

--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -52,17 +52,13 @@ export default defineComponent({
 
 <template>
   <NavBar/>
-  <form @submit.prevent="submitSearch" class="row g-3">
-      <div class="col-auto">
-          <span>Search</span><br>
-          <input v-model="searchQuery" class="form-control form-control-lg" placeholder="Busca un producto"/><br>
-      </div>
-
-      <div class="col-auto">
-          <button type="submit" class="btn btn-primary mb-3">Search</button>
-      </div>
+  <form @submit.prevent="submitSearch">
+    <div class="input-group search-form">
+      <input v-model="searchQuery" type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+      <button type="button" class="btn btn-dark">search</button>
+    </div>
   </form>
-  <div class="product-list-view--products container">
+  <div class="product-list-view--products container item-list">
     <div class="product" v-for="product of products" :key="product">
       <ProductCard 
         :description=product.description
@@ -75,87 +71,21 @@ export default defineComponent({
   </div>
 </template>
 
-<style lang="scss">
-.list-container {
-  padding: 10px;
-  &.list {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    & > * {
-      flex: 1 0 100%;
-    }
-    .list-item {
-      border: 1px solid lightgrey;
-      height: 60px;
-      margin: 10px 0;
-      display: flex;
-      flex-direction: row;
-      flex-wrap: none;
-    }
-    .list-item-image-container {
-      width: 60px;
-      flex: none;
-    }
-    .list-item-image {
-      width: 40px;
-      height: 40px;
-      margin: 10px;
-      object-fit: contain;
-    }
-    .list-item-data-container {
-      flex: 1 0 auto;
-    }
-  }
-  &.grid {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    & > * {
-      flex: none;
-    }
-  }
-}
-.list-item {
-  border: 1px solid lightgrey;
-  height: 260px;
-  width: 200px;
-  margin: 10px 0;
+<style scoped>
+.item-list {
   display: flex;
-  flex-direction: column;
-  flex-wrap: none;
+  flex-flow: row wrap;
+  width: 95%;
+  gap: 1rem 1rem;
 }
-.list-item-data-container {
-  background-color: lightgrey;
+
+.search{
+  font-weight: 700;
+}
+
+.search-form{
+  max-width: 50%;
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  height: 60px;
-  padding: 5px;
-  & > * {
-    height: 30px;
-  }
-}
-.list-item-data-name {
-  font-weight: bold;
-  text-decoration: none;
-  color: black;
-  font-size: 16px;
-}
-.list-item-data-details {
-  font-weight: normal;
-  font-size: 14px;
-}
-.list-item-image-container {
-  width: 200px;
-  height: 200px;
-}
-.list-item-image {
-  width: 180px;
-  height: 200px;
-  margin: 0 10px;
-  object-fit: contain;
+  margin: 5% 20% 5%;
 }
 </style>
